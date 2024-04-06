c97bb15d933c60626c3becb2daaeed8f52fec4ce
Roderick Lan <rodericklan03@gmail.com>
2024-04-05 22:24:43 -0600

    reconstruction preprocessing

--- reconstruction.py
+++ reconstruction.py
@@ -11,22 +11,39 @@ class Reconstruction:
         self.init_poly = o3d.io.read_point_cloud(self.src)
         
 
-    def preproc(self, type="stat"):
-        sz = .02
-
-        pcd = self.init_poly.voxel_down_sample(sz)
-
+    def preproc(self, type="stat", down_sample=False, pcd=None, **rargs):
+        if pcd is None:
+            pcd = self.init_poly
+        opcd = pcd
+        
+        if down_sample:
+            # sz = .02
+            try:
+                sz = rargs["sz"]
+            except:
+                print("Size argument for downsampling, 'sz', not given, using default 0.02 (0.05 recommended if you want more sparse result)")
+                sz = .02
+            pcd = pcd.voxel_down_sample(sz)
+        
+        
         if type == "stat":
-            n_neigh = 5
-            std_ratio = 0.25
-            _, ind = self.init_poly.remove_statistical_outlier(n_neigh, std_ratio, False)
+            # n_neigh = 5
+            # std_ratio = 0.25
+            n_neigh, std_ratio = rargs["n_neigh"], rargs["std_ratio"]
+            _, ind = pcd.remove_statistical_outlier(n_neigh, std_ratio, False)
 
-            
+        elif type == "rad":
+            # n_pts = 12
+            # rad = .15
+            n_pts, rad = rargs["n_pts"], rargs["rad"]
+            _, ind = pcd.remove_radius_outlier(n_pts, rad)
+
         else:
-            n_pts = 12
-            rad = .15
-            _, ind = self.init_poly.remove_radius_outlier(n_pts, rad)
-
+            print("Invalid type, no outliers removed. \nValid type values are:\n'stat' for statistical outlier removal and\n'rad' for radius outlier removal")
+            ind = None
+        
+        pcd = pcd.select_by_index(ind)
+        return pcd, ind, opcd
         
 
 
